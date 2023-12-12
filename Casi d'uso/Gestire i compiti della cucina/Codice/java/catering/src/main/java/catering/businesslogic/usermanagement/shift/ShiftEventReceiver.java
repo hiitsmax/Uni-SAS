847496e621ea4e7bd3f9c23add5369da1ced5ecc
Massimiliano Bettarello <117319272+MaxiToTheMoon@users.noreply.github.com>
2023-12-12 22:42:27 +0100

    Update ShiftEventReceiver.java

--- Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/usermanagement/shift/ShiftEventReceiver.java
+++ Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/usermanagement/shift/ShiftEventReceiver.java
@@ -1,5 +1,9 @@
 package catering.businesslogic.usermanagement.shift;
 
 public class ShiftEventReceiver {
-    
+    public void updateShiftCreated(Shift sh);
+
+    public void updateShiftDeleted(Shift sh);
+
+    public void updateShiftModified(Shift sh);
 }
