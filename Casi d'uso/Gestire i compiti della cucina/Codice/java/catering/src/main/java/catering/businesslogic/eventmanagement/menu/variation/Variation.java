afab7480c761d6d21b781a9c5fedba749a4ac0fe
MX <mendozza.massimo@gmail.com>
2023-12-12 19:22:04 +0100

    struttura variation

--- Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/eventmanagement/menu/variation/Variation.java
+++ Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/eventmanagement/menu/variation/Variation.java
@@ -1,5 +1,35 @@
 package catering.businesslogic.eventmanagement.menu.variation;
 
+import catering.businesslogic.eventmanagement.menu.menuitem.MenuItem;
+
 public class Variation {
+    private MenuItem item; // da cambiare in dcd
+    private VariationType type; // da cambiare in dcd
+    private boolean isAccepted; // da cambiare in dcd
+
+    public Variation(MenuItem item, VariationType type, boolean isAccepted) {
+        this.item = item;
+        this.type = type;
+        this.isAccepted = isAccepted;
+    }
+
+    public MenuItem getItem() {
+        return item;
+    }
+    public void setItem(MenuItem item) {
+        this.item = item;
+    }
+    public VariationType getType() {
+        return type;
+    }
+    public void setType(VariationType type) {
+        this.type = type;
+    }
+    public boolean isAccepted() {
+        return isAccepted;
+    }
+    public void setAccepted(boolean isAccepted) {
+        this.isAccepted = isAccepted;
+    }
     
 }
