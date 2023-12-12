6bd8dcc52ae706d47e4121ed0fd92171542bf04a
Massimiliano Bettarello <117319272+MaxiToTheMoon@users.noreply.github.com>
2023-12-12 22:36:00 +0100

    Update Shift.java (da completare)

--- Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/usermanagement/shift/Shift.java
+++ Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/usermanagement/shift/Shift.java
@@ -1,5 +1,31 @@
 package catering.businesslogic.usermanagement.shift;
 
 public class Shift {
+    private String start;
+    private String end;
+    private String type;
+    private int limit;
+
+    public Shift(String start, String end, String type, int limit) {
+        this.start = start;
+        this.end = end;
+        this.type = type;
+        this.limit = limit;
+    }
     
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setLimit(String limit) {
+        this.limit = limit;
+    }
 }
