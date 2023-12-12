8804ef5cbe3b0c84e33779bd8218e9360502ae86
MX <mendozza.massimo@gmail.com>
2023-12-12 19:14:53 +0100

    struttura penal

--- Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/eventmanagement/event/penal/Penal.java
+++ Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/eventmanagement/event/penal/Penal.java
@@ -1,5 +1,26 @@
 package catering.businesslogic.eventmanagement.event.penal;
 
 public class Penal {
+    private int sum;
+    private String description;
+
+    public Penal(int sum, String description) {
+        this.sum = sum;
+        this.description = description;
+    }
+    
+    public int getSum() {
+        return sum;
+    }
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     
 }
