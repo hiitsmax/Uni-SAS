9ed1dad4e0fb51f30d3c231ea98f5026b897b534
Massimiliano Bettarello <117319272+MaxiToTheMoon@users.noreply.github.com>
2023-12-12 22:21:37 +0100

    Update EventEventReceiver.java

--- Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/eventmanagement/event/EventEventReceiver.java
+++ Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/businesslogic/eventmanagement/event/EventEventReceiver.java
@@ -1,5 +1,21 @@
 package catering.businesslogic.eventmanagement.event;
 
 public class EventEventReceiver {
+     public void updateEventCreated(Event e);
+
+    public void updateEventDeleted(Event e);
+
+    public void updateEventModified(Event e);
+
+    public void updateRecurrencyCreated(Recurrency r);
+
+    public void updateRecurrencyDeleted(Recurrency r);
+
+    public void updateRecurrencyModified(Recurrency r);
     
+    public void updateServiceCreated(Service serv);
+
+    public void updateServiceDeleted(Service serv);
+
+    public void updateServiceModified(Service serv);
 }
