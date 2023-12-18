277301382e5d337b3bd0372e28c7bed1304bce95
MX <mendozza.massimo@gmail.com>
2023-12-18 22:15:31 +0100

    creato scheletro test prof

---
+++ Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/tests/kitchentasks/TestCatERing1.java
@@ -0,0 +1,23 @@
+package catering.tests.kitchentasks;
+import catering.businesslogic.CatERing;
+import catering.businesslogic.UseCaseLogicException;
+import catering.businesslogic.eventmanagement.event.Event;
+import catering.businesslogic.eventmanagement.menu.Menu;
+import catering.businesslogic.eventmanagement.menu.MenuException;
+import catering.businesslogic.eventmanagement.menu.section.Section;
+import catering.businesslogic.eventmanagement.service.ServiceInfo;
+import catering.businesslogic.kitchenmanagement.recipe.Recipe;
+import javafx.collections.ObservableList;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class TestCatERing1 {
+    public static void main(String[] args) {
+        try {
+
+        } catch (UseCaseLogicException | MenuException e) {
+            System.out.println("Errore di logica nello use case");
+        }
+    }
+}
