e040b2a87557f6146f3c74d76a31c675178d8680
unknown <massimiliano.bettarello@gmail.com>
2023-12-28 17:42:51 +0100

    event receiver and minor fixes

---
+++ Casi d'uso/Gestire i compiti della cucina/Codice/java/catering/src/main/java/catering/persistence/EventPersistence.java
@@ -0,0 +1,62 @@
+package catering.persistence;
+
+import java.sql.SQLException;
+
+import catering.businesslogic.eventmanagement.event.Event;
+import catering.businesslogic.eventmanagement.event.EventEventReceiver;
+import catering.businesslogic.eventmanagement.event.recurrency.Recurrency;
+import catering.businesslogic.eventmanagement.service.Service;
+
+public class EventPersistence implements EventEventReceiver {
+    
+    @Override
+    public void updateEventCreated(Event e) {
+        Event.saveNewEvent(e);
+    }
+
+    @Override
+    public void updateEventDeleted(Event e) {
+        Event.deleteMenu(e);
+    }
+
+    @Override
+    public void updateEventModified(Event e) {
+        Event.saveEvent(e);
+    }
+
+    @Override
+    public void updateRecurrencyCreated(Recurrency r) {
+        try {
+            Recurrency.saveNewRecurrency(r);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateRecurrencyDeleted(Recurrency r) {
+        Recurrency.deleteRecurrency(r);
+    }
+
+    @Override
+    public void updateRecurrencyModified(Recurrency r) {
+        Recurrency.saveRecurrency(r); 
+    }
+
+    @Override
+    public void updateServiceCreated(Service s) {
+        Service.saveNewService(s);
+    }
+
+    @Override
+    public void updateServiceModified(Service s) {
+        Service.saveService();
+    }
+
+    @Override
+    public void updateServiceDeleted(Service s) {
+        Service.deleteService();
+    }
+
+}
